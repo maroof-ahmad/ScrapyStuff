d61f539b87fa861065f7fc0e5970c97836f6a46b
Maroof Ahmad <ahmadmaroof8@gmail.com>
2016-09-28 23:08:59 +0530

    code size reduced

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -3,6 +3,11 @@ import scrapy
 class QuotesSpider(scrapy.Spider):
     name = 'quotes'
 
+    # shortcut to start_requests
+    # start_urls = [
+    #     'http://quotes.toscrape.com/page/1/',
+    #     'http://quotes.toscrape.com/page/2/',
+    # ]
     def start_requests(self):
         urls = [
             'http://quotes.toscrape.com/page/1/',
@@ -12,8 +17,19 @@ class QuotesSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback = self.parse)
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
-        with open(filename,'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
+        # page = response.url.split("/")[-2]
+        # filename = 'quotes-%s.html' % page
+        # with open(filename,'wb') as f:
+        #     f.write(response.body)
+        # self.log('Saved file %s' % filename)
+        for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').extract_first(),
+                'author': quote.css('span small::text').extract_first(),
+                'tags': quote.css('div.tags a.tag::text').extract(),
+            }
+
+        next_page = response.css('li.next a::attr(href)').extract_first()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
